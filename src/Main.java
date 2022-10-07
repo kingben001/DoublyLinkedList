9915088e6c5527e39926329b98928a4f961ff924
kingben001 <kingben001@yahoo.com>
2022-10-07 19:54:33 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+
+
+
+  // myDLL.remove(1);
+
+        myDLL.printList();
+
+
+
+
+    }
+
+}
